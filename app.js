4650488dd7daf31ebcbaa7f973126d3985b22e8b
RodrigoF32 <rodrigofreitas3223@outlook.com>
2022-11-10 10:46:03 -0300

    Mudança feita pelo Paulo

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se canectando ao banco de dados")
+// mais um console log de teste
+console.log("o Paulo fex esse commit sem você ver na aula")
